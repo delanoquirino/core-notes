1e56e1b125d35a99b604f229b525ba34baedb0c3
delanoquirino <delanoquirino@gmail.com>
2024-01-18 20:30:13 -0300

    feat: create AddTaskForm component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,9 @@
+import { AddTaskForm } from "@/components/AddTaskForm";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     Home
+    <main className="flex min-h-screen flex-col items-center justify-between bg-[boder-[#D9D9D9]">
+     <AddTaskForm/>
     </main>
   )
 }
---
+++ frontend/src/components/AddTaskForm.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { IoIosStarOutline } from "react-icons/io";
+
+export const AddTaskForm = () => {
+  return (
+    <form className="border-[2px] w-[90%] md:w-[60%] lg:w-[40%] mx-auto border-[#D9D9D9] shadow-lg shadow-gray-500 bg-white rounded-3xl md:rounded-none mt-6 ">
+      <div className="py-3 px-6 flex gap-4">
+        <input
+          type="text"
+          placeholder="Título"
+          className="w-full placeholder:text-black outline-none bg-transparent font-bold text-sm"
+        />
+        <div>
+            <input
+              type="checkbox"
+              className="hidden"
+              id="starCheckbox"
+            />
+            <label htmlFor="starCheckbox" className="cursor-pointer">
+              <IoIosStarOutline className="text-2xl mr-2" />
+            </label>
+        </div>
+      </div>
+      <div className="border-t-2 p-2"><textarea className="w-full px-2 py-1 outline-none resize-none text-xs" placeholder="Criar Nota..."/></div>
+    </form>
+  );
+};
