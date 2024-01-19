d0fb9a042a0af5cde74f5129fb9f3edf8134e972
delanoquirino <delanoquirino@gmail.com>
2024-01-18 21:54:01 -0300

    style: grid cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,19 @@
 import { AddTaskForm } from "@/components/AddTaskForm";
+import { CardTask } from "@/components/CardTask";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-[boder-[#D9D9D9]">
-     <AddTaskForm/>
+    <main className="flex min-h-screen flex-col items-center bg-[#F0F2F5]">
+      <div className="container p-4">
+        <AddTaskForm />
+        <div className=" mt-9">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-11">
+            <CardTask />
+            <CardTask />
+            <CardTask />
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
