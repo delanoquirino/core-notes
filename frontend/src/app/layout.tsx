62b6371892affc4400807448ad4e2f5d89327926
delanoquirino <delanoquirino@gmail.com>
2024-01-19 19:20:43 -0300

    style: whitespace

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header/>
+         <Header/>
         {children}
       </body>
     </html>
