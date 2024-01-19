459a1e81b39a3f687567d19e39140da2d620b477
delanoquirino <delanoquirino@gmail.com>
2024-01-18 22:01:46 -0300

    style: reducing the gap

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="container p-4">
         <AddTaskForm />
         <div className=" mt-9">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-11">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             <CardTask />
             <CardTask />
             <CardTask />
