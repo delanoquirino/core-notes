cea6c88484dc289dea7dae39493295a4f2d3813f
delanoquirino <delanoquirino@gmail.com>
2024-01-19 19:18:03 -0300

    feat: fetching data from the api

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,41 @@
+"use client"
 import { AddTaskForm } from "@/components/AddTaskForm";
-import { CardTask } from "@/components/CardTask";
+import { Grid } from "@/components/Grid";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+
 
 export default function Home() {
+  const [tasks,setTasks] = useState([]);
+  const [onEdit,setOnEdit] = useState(null)
+  
+  //buscar dados na api
+  const getTasks = async () => {
+    try{
+      const res = await axios.get("http://localhost:8000");
+      setTasks(res.data.sort((a: { title: number; },b: { title: number; }) => (a.title > b.title ? 1: -1)))
+    } catch ({error}:any) {
+      toast.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getTasks();
+  }, [setTasks])
+
+
   return (
+    <>
+    <ToastContainer autoClose={3000} position={"bottom-left"} />
+       
     <main className="flex min-h-screen flex-col items-center bg-[#F0F2F5]">
-      <div className="container p-4">
+      <div className="container mx-auto px-4">
         <AddTaskForm />
-        <div className=" mt-9">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            <CardTask />
-            <CardTask />
-            <CardTask />
-          </div>
-        </div>
+       <Grid tasks={tasks} />
       </div>
     </main>
+    </>
   );
 }
