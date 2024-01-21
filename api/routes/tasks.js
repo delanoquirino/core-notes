0af9f7ffe442a0a5ae5cdfc559fc976676b7dfb0
delanoquirino <delanoquirino@gmail.com>
2024-01-21 16:49:16 -0300

    feat: Put route to update card color

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {addTask, deleteTask, getTask, updateTask} from "../controllers/task.js"
+import {addTask, deleteTask, getTask, updateTask, updateTaskColor} from "../controllers/task.js"
 
 const router = express.Router()
 
@@ -10,5 +10,7 @@ router.post("/", addTask)
 router.put("/:id", updateTask)
 
 router.delete("/:id", deleteTask)
+
+router.put('/tasks/:id/color', updateTaskColor);
 
 export default router
