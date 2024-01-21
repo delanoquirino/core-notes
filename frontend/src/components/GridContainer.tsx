65ff505925b366d01278732d7a87cd8b69756243
delanoquirino <delanoquirino@gmail.com>
2024-01-21 18:16:04 -0300

    feat: adding GridContainer component

---
+++ frontend/src/components/GridContainer.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+
+interface GridContainerProps {
+    children: ReactNode;
+  }
+export const GridContainer = ({ children }:GridContainerProps) => {
+    return (
+      <div className="pt-9">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {children}
+        </div>
+      </div>
+    );
+  };
+  
