bada15bffde1845c0149f3ede6d2b12b45b27089
delanoquirino <delanoquirino@gmail.com>
2024-01-21 18:16:55 -0300

    style: adding GridContainer component to home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { AddTaskForm } from "@/components/AddTaskForm";
 import { CardTask } from "@/components/CardTask";
+import { GridContainer } from "@/components/GridContainer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -8,14 +9,13 @@ export default function Home() {
   return (
     <>
       <ToastContainer autoClose={3000} position={"bottom-left"} />
-      <main className="bg-[#F0F2F5] min-h-screen">
+      <main className="bg-[#F0F2F5] min-h-screen pt-20">
         <div className="max-w-[1440px] mx-auto px-4 py-4">
           <AddTaskForm />
-          <div className=" pt-9">
-            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-              <CardTask />
-            </div>
-          </div>
+
+          <GridContainer>
+            <CardTask />
+          </GridContainer>
         </div>
       </main>
     </>
