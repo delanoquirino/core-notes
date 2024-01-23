17df2bb1db06cd6476bfd5191e6e5a3efd42d4ae
delanoquirino <delanoquirino@gmail.com>
2024-01-23 18:41:08 -0300

    feat: adding types.tsx to store types

--- frontend/src/types/types.tsx
+++ frontend/src/types/types.tsx
@@ -6,12 +6,14 @@ export type TaskType = {
   bgcolor: string;
 };
 
-export interface TaskContextProps {
+export type TaskContextProps = {
   tasks: TaskType[];
   setTasks: React.Dispatch<React.SetStateAction<TaskType[]>>;
+  onEdit: TaskType | null;
   setOnEdit: React.Dispatch<React.SetStateAction<TaskType | null>>;
+  getTasks: () => Promise<void>;
   loading: boolean;
-}
+};
 
 export type Inputs = {
   title: string;
@@ -19,12 +21,6 @@ export type Inputs = {
   favorite: number;
   bgcolor: string;
 };
-
-export interface TaskContextFormProps {
-  onEdit: TaskType | null;
-  setOnEdit: React.Dispatch<React.SetStateAction<TaskType | null>>;
-  getTasks: () => Promise<void>;
-}
 
 export type GridContainerProps = {
   children: React.ReactNode;
