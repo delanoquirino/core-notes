e79df5fe45c59d0432c84e10f8ad159f9535ef86
delanoquirino <delanoquirino@gmail.com>
2024-01-23 18:43:02 -0300

    feat: creating index files of file exports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+export * from "./AddTaskForm";
+export * from "./ListTasks";
+export * from "./EditBgColor";
+export * from "./GridContainer";
+export * from "./Header";
+export * from "./Loading";
+export * from "./CardTasks";
