0a246cef2f7bad9b2232e513e48c905a677dace3
delanoquirino <delanoquirino@gmail.com>
2024-01-22 21:01:37 -0300

    fix: remove gridcontainer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 import { AddTaskForm } from "@/components/AddTaskForm";
 import { CardTask } from "@/components/CardTask";
-import { GridContainer } from "@/components/GridContainer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
     <>
-      <ToastContainer autoClose={3000} position={"bottom-left"} />
+      <ToastContainer autoClose={2000} position={"bottom-left"} />
       <main className="bg-[#F0F2F5] min-h-screen pt-20">
         <div className="max-w-[1440px] mx-auto px-4 py-4">
           <AddTaskForm />
-
-          <GridContainer>
-            <CardTask />
-          </GridContainer>
+          <CardTask />
         </div>
       </main>
     </>
